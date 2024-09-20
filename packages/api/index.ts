707024719142d3481487a0c49bc5450bdec2ee2a
Rocio <111855553+Rogarrid@users.noreply.github.com>
2024-09-20 08:40:26 +0200

    created a monorepo and set up a new Fastify server

---
+++ packages/api/index.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`Server listening at ${address}`);
+});
