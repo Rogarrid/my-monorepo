74b51c9e772a18bcafaacf5323310bfe40415219
Rocio <rocio.garrido@es.abb.com>
2024-09-24 18:18:47 +0200

    fixed test

--- packages/api/test/userController.test.ts
+++ packages/api/test/userController.test.ts
@@ -93,7 +93,7 @@ describe("userController", () => {
         send: jest.fn(),
       } as unknown as FastifyReply;
 
-      const mockResponse = { message: "Logged in successfully" };
+      const mockResponse = { user: undefined, accessToken: undefined };
       (signIn as jest.Mock).mockResolvedValueOnce(mockResponse);
 
       await userController.login(request, reply);
@@ -102,250 +102,254 @@ describe("userController", () => {
       expect(reply.code).toHaveBeenCalledWith(200);
       expect(reply.send).toHaveBeenCalledWith(mockResponse);
     });
+  });
 
-    it("should handle errors", async () => {
-      const request = {
-        body: {
-          email: "test@gmail.com",
-          password: "testpassword",
-        },
-      } as FastifyRequest<{ Body: LoginDto }>;
+  it("should handle errors", async () => {
+    const request = {
+      body: {
+        email: "test@gmail.com",
+        password: "testpassword",
+      },
+    } as FastifyRequest<{ Body: LoginDto }>;
 
-      const reply = {
-        code: jest.fn().mockReturnThis(),
-        send: jest.fn(),
-      } as unknown as FastifyReply;
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as FastifyReply;
 
-      const error = new Error("Login error");
-      (signIn as jest.Mock).mockRejectedValueOnce(error);
+    const error = new Error("Login error");
+    (signIn as jest.Mock).mockRejectedValueOnce(error);
 
-      await userController.login(request, reply);
+    await userController.login(request, reply);
 
-      expect(reply.code).toHaveBeenCalledWith(500);
-      expect(reply.send).toHaveBeenCalledWith({
-        code: 500,
-        message: "Login error",
-      });
+    expect(reply.code).toHaveBeenCalledWith(500);
+    expect(reply.send).toHaveBeenCalledWith({
+      code: 500,
+      message: "Login error",
+    });
+  });
+});
+
+describe("get", () => {
+  it("should retrieve a user by ID", async () => {
+    const request = {
+      params: { id: "1" },
+    } as FastifyRequest<{ Params: UserIdDto }>;
+
+    const reply = {
+      send: jest.fn(),
+      code: jest.fn().mockReturnThis(),
+    } as unknown as FastifyReply;
+
+    const mockUser = { id: 1, username: "testuser" };
+    (getUser as jest.Mock).mockResolvedValueOnce(mockUser);
+
+    await userController.get(request, reply);
+
+    expect(getUser).toHaveBeenCalledWith(1);
+    expect(reply.send).toHaveBeenCalledWith(mockUser);
+  });
+
+  it("should return 404 if user not found", async () => {
+    const request = {
+      params: { id: "1" },
+    } as FastifyRequest<{ Params: UserIdDto }>;
+
+    const reply = {
+      send: jest.fn(),
+      code: jest.fn().mockReturnThis(),
+    } as unknown as FastifyReply;
+
+    (getUser as jest.Mock).mockResolvedValueOnce(null);
+
+    await userController.get(request, reply);
+
+    expect(reply.code).toHaveBeenCalledWith(404);
+    expect(reply.send).toHaveBeenCalledWith({ message: "User not found" });
+  });
+
+  it("should handle errors", async () => {
+    const request = {
+      params: { id: "1" },
+    } as FastifyRequest<{ Params: UserIdDto }>;
+
+    const reply = {
+      send: jest.fn(),
+      code: jest.fn().mockReturnThis(),
+    } as unknown as FastifyReply;
+
+    const error = new Error("Error fetching user");
+    (getUser as jest.Mock).mockRejectedValueOnce(error);
+
+    await userController.get(request, reply);
+
+    expect(reply.code).toHaveBeenCalledWith(500);
+    expect(reply.send).toHaveBeenCalledWith({
+      code: 500,
+      message: "Error fetching user",
+    });
+  });
+});
+
+describe("update", () => {
+  it("should update a user", async () => {
+    const request = {
+      params: { id: "1" },
+      body: {
+        name: "Updated User",
+        email: `updated_${Date.now()}@example.com`,
+        role: "user",
+        password: "newpassword123",
+      } as UpdateUserDto,
+    } as FastifyRequest<{ Params: UserIdDto; Body: UpdateUserDto }>;
+
+    const reply = {
+      send: jest.fn(),
+    } as unknown as FastifyReply;
+
+    const mockUser = { id: 1, username: "updatedUser" };
+    (updateUser as jest.Mock).mockResolvedValueOnce(mockUser);
+
+    await userController.update(request, reply);
+
+    expect(updateUser).toHaveBeenCalledWith(1, request.body);
+    expect(reply.send).toHaveBeenCalledWith(mockUser);
+  });
+
+  it("should handle errors", async () => {
+    const request = {
+      params: { id: "1" },
+      body: {
+        name: "Updated User",
+        email: `updated_${Date.now()}@example.com`,
+        role: "user",
+        password: "newpassword123",
+      } as UpdateUserDto,
+    } as FastifyRequest<{ Params: UserIdDto; Body: UpdateUserDto }>;
+
+    const reply = {
+      send: jest.fn(),
+      code: jest.fn().mockReturnThis(),
+    } as unknown as FastifyReply;
+
+    const error = new Error("Error updating user");
+    (updateUser as jest.Mock).mockRejectedValueOnce(error);
+
+    await userController.update(request, reply);
+
+    expect(reply.code).toHaveBeenCalledWith(500);
+    expect(reply.send).toHaveBeenCalledWith({
+      code: 500,
+      message: "Error updating user",
+    });
+  });
+});
+
+describe("delete", () => {
+  it("should delete a user", async () => {
+    const request = {
+      params: { id: "1" },
+    } as FastifyRequest<{ Params: UserIdDto }>;
+
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as FastifyReply;
+
+    await userController.delete(request, reply);
+
+    expect(deleteUser).toHaveBeenCalledWith(1);
+    expect(reply.code).toHaveBeenCalledWith(200);
+    expect(reply.send).toHaveBeenCalledWith({ message: "User deleted" });
+  });
+
+  it("should handle errors", async () => {
+    const request = {
+      params: { id: "1" },
+    } as FastifyRequest<{ Params: UserIdDto }>;
+
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as FastifyReply;
+
+    const error = new Error("Error deleting user");
+    (deleteUser as jest.Mock).mockRejectedValueOnce(error);
+
+    await userController.delete(request, reply);
+
+    expect(reply.code).toHaveBeenCalledWith(500);
+    expect(reply.send).toHaveBeenCalledWith({
+      code: 500,
+      message: "Error deleting user",
+    });
+  });
+});
+
+describe("refreshToken", () => {
+  it("should refresh the access token", async () => {
+    const request = {
+      body: {
+        refreshToken: "mockRefreshToken",
+      },
+    } as FastifyRequest<{ Body: { refreshToken: string } }>;
+
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as FastifyReply;
+
+    const mockResponse = {
+      newAccessToken: undefined,
+      refreshToken: "mockRefreshToken",
+    };
+    (refreshTokenService as jest.Mock).mockResolvedValueOnce(mockResponse);
+
+    await userController.refreshToken(request, reply);
+
+    expect(refreshTokenService).toHaveBeenCalledWith(request.body.refreshToken);
+    expect(reply.send).toHaveBeenCalledWith(mockResponse);
+  });
+
+  it("should return 400 if refresh token is not provided", async () => {
+    const request = {
+      body: {},
+    } as FastifyRequest<{ Body: RefreshTokenDto }>;
+
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as FastifyReply;
+
+    await userController.refreshToken(request, reply);
+
+    expect(reply.code).toHaveBeenCalledWith(400);
+    expect(reply.send).toHaveBeenCalledWith({
+      message: "Refresh token not found",
     });
   });
 
-  describe("get", () => {
-    it("should retrieve a user by ID", async () => {
-      const request = {
-        params: { id: "1" },
-      } as FastifyRequest<{ Params: UserIdDto }>;
+  it("should handle errors", async () => {
+    const request = {
+      body: {
+        refreshToken: "mockRefreshToken",
+      },
+    } as FastifyRequest<{ Body: RefreshTokenDto }>;
 
-      const reply = {
-        send: jest.fn(),
-        code: jest.fn().mockReturnThis(),
-      } as unknown as FastifyReply;
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as FastifyReply;
 
-      const mockUser = { id: 1, username: "testuser" };
-      (getUser as jest.Mock).mockResolvedValueOnce(mockUser);
+    const error = new Error("Error refreshing token");
+    (refreshTokenService as jest.Mock).mockRejectedValueOnce(error);
 
-      await userController.get(request, reply);
+    await userController.refreshToken(request, reply);
 
-      expect(getUser).toHaveBeenCalledWith(1);
-      expect(reply.send).toHaveBeenCalledWith(mockUser);
-    });
-
-    it("should return 404 if user not found", async () => {
-      const request = {
-        params: { id: "1" },
-      } as FastifyRequest<{ Params: UserIdDto }>;
-
-      const reply = {
-        send: jest.fn(),
-        code: jest.fn().mockReturnThis(),
-      } as unknown as FastifyReply;
-
-      (getUser as jest.Mock).mockResolvedValueOnce(null);
-
-      await userController.get(request, reply);
-
-      expect(reply.code).toHaveBeenCalledWith(404);
-      expect(reply.send).toHaveBeenCalledWith({ message: "User not found" });
-    });
-
-    it("should handle errors", async () => {
-      const request = {
-        params: { id: "1" },
-      } as FastifyRequest<{ Params: UserIdDto }>;
-
-      const reply = {
-        send: jest.fn(),
-        code: jest.fn().mockReturnThis(),
-      } as unknown as FastifyReply;
-
-      const error = new Error("Error fetching user");
-      (getUser as jest.Mock).mockRejectedValueOnce(error);
-
-      await userController.get(request, reply);
-
-      expect(reply.code).toHaveBeenCalledWith(500);
-      expect(reply.send).toHaveBeenCalledWith({
-        code: 500,
-        message: "Error fetching user",
-      });
-    });
-  });
-
-  describe("update", () => {
-    it("should update a user", async () => {
-      const request = {
-        params: { id: "1" },
-        body: {
-          name: "Updated User",
-          email: `updated_${Date.now()}@example.com`,
-          role: "user",
-          password: "newpassword123",
-        } as UpdateUserDto,
-      } as FastifyRequest<{ Params: UserIdDto; Body: UpdateUserDto }>;
-
-      const reply = {
-        send: jest.fn(),
-      } as unknown as FastifyReply;
-
-      const mockUser = { id: 1, username: "updatedUser" };
-      (updateUser as jest.Mock).mockResolvedValueOnce(mockUser);
-
-      await userController.update(request, reply);
-
-      expect(updateUser).toHaveBeenCalledWith(1, request.body);
-      expect(reply.send).toHaveBeenCalledWith(mockUser);
-    });
-
-    it("should handle errors", async () => {
-      const request = {
-        params: { id: "1" },
-        body: {
-          name: "Updated User",
-          email: `updated_${Date.now()}@example.com`,
-          role: "user",
-          password: "newpassword123",
-        } as UpdateUserDto,
-      } as FastifyRequest<{ Params: UserIdDto; Body: UpdateUserDto }>;
-
-      const reply = {
-        send: jest.fn(),
-        code: jest.fn().mockReturnThis(),
-      } as unknown as FastifyReply;
-
-      const error = new Error("Error updating user");
-      (updateUser as jest.Mock).mockRejectedValueOnce(error);
-
-      await userController.update(request, reply);
-
-      expect(reply.code).toHaveBeenCalledWith(500);
-      expect(reply.send).toHaveBeenCalledWith({
-        code: 500,
-        message: "Error updating user",
-      });
-    });
-  });
-
-  describe("delete", () => {
-    it("should delete a user", async () => {
-      const request = {
-        params: { id: "1" },
-      } as FastifyRequest<{ Params: UserIdDto }>;
-
-      const reply = {
-        code: jest.fn().mockReturnThis(),
-        send: jest.fn(),
-      } as unknown as FastifyReply;
-
-      await userController.delete(request, reply);
-
-      expect(deleteUser).toHaveBeenCalledWith(1);
-      expect(reply.code).toHaveBeenCalledWith(200);
-      expect(reply.send).toHaveBeenCalledWith({ message: "User deleted" });
-    });
-
-    it("should handle errors", async () => {
-      const request = {
-        params: { id: "1" },
-      } as FastifyRequest<{ Params: UserIdDto }>;
-
-      const reply = {
-        code: jest.fn().mockReturnThis(),
-        send: jest.fn(),
-      } as unknown as FastifyReply;
-
-      const error = new Error("Error deleting user");
-      (deleteUser as jest.Mock).mockRejectedValueOnce(error);
-
-      await userController.delete(request, reply);
-
-      expect(reply.code).toHaveBeenCalledWith(500);
-      expect(reply.send).toHaveBeenCalledWith({
-        code: 500,
-        message: "Error deleting user",
-      });
-    });
-  });
-
-  describe("refreshToken", () => {
-    it("should refresh the access token", async () => {
-      const request = {
-        body: {
-          refreshToken: "mockRefreshToken",
-        },
-      } as FastifyRequest<{ Body: RefreshTokenDto }>;
-
-      const reply = {
-        send: jest.fn(),
-      } as unknown as FastifyReply;
-
-      const mockAccessToken = "mockAccessToken";
-      (refreshTokenService as jest.Mock).mockResolvedValueOnce(mockAccessToken);
-
-      await userController.refreshToken(request, reply);
-
-      expect(refreshTokenService).toHaveBeenCalledWith("mockRefreshToken");
-      expect(reply.send).toHaveBeenCalledWith({ accessToken: mockAccessToken });
-    });
-
-    it("should return 400 if refresh token is not provided", async () => {
-      const request = {
-        body: {},
-      } as FastifyRequest<{ Body: RefreshTokenDto }>;
-
-      const reply = {
-        code: jest.fn().mockReturnThis(),
-        send: jest.fn(),
-      } as unknown as FastifyReply;
-
-      await userController.refreshToken(request, reply);
-
-      expect(reply.code).toHaveBeenCalledWith(400);
-      expect(reply.send).toHaveBeenCalledWith({
-        message: "Refresh token not found",
-      });
-    });
-
-    it("should handle errors", async () => {
-      const request = {
-        body: {
-          refreshToken: "mockRefreshToken",
-        },
-      } as FastifyRequest<{ Body: RefreshTokenDto }>;
-
-      const reply = {
-        code: jest.fn().mockReturnThis(),
-        send: jest.fn(),
-      } as unknown as FastifyReply;
-
-      const error = new Error("Error refreshing token");
-      (refreshTokenService as jest.Mock).mockRejectedValueOnce(error);
-
-      await userController.refreshToken(request, reply);
-
-      expect(reply.code).toHaveBeenCalledWith(500);
-      expect(reply.send).toHaveBeenCalledWith({
-        code: 500,
-        message: "Error refreshing token",
-      });
+    expect(reply.code).toHaveBeenCalledWith(500);
+    expect(reply.send).toHaveBeenCalledWith({
+      code: 500,
+      message: "Error refreshing token",
     });
   });
 });
--- packages/api/test/userService.test.ts
+++ packages/api/test/userService.test.ts
@@ -1,101 +1,101 @@
-// import { PrismaClient } from "@prisma/client";
-// import bcrypt from "bcrypt";
-// import {
-//   signUp,
-//   signIn,
-//   getUser,
-//   updateUser,
-//   deleteUser,
-//   refreshTokenService,
-// } from "../../services/userService";
-// const prisma = new PrismaClient();
-// const secretKey = process.env.JWT_SECRET || "test_secret";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import {
+  signUp,
+  signIn,
+  getUser,
+  updateUser,
+  deleteUser,
+  refreshTokenService,
+} from "../../services/userService";
+const prisma = new PrismaClient();
+const secretKey = process.env.JWT_SECRET || "test_secret";
 
-// describe("User Service", () => {
-//   let userId: number;
+describe("User Service", () => {
+  let userId: number;
 
-//   const generateUniqueEmail = () => `test_${Date.now()}@example.com`;
+  const generateUniqueEmail = () => `test_${Date.now()}@example.com`;
 
-//   beforeEach(async () => {
-//     const userData = {
-//       name: "Test User",
-//       email: generateUniqueEmail(),
-//       password: "password123",
-//       role: "user",
-//     };
-//     const response = await signUp(userData);
-//     userId = response.user.id;
-//   });
+  beforeEach(async () => {
+    const userData = {
+      name: "Test User",
+      email: generateUniqueEmail(),
+      password: "password123",
+      role: "user",
+    };
+    const response = await signUp(userData);
+    userId = response.user.id;
+  });
 
-//   afterAll(async () => {
-//     await prisma.user.deleteMany();
-//     await prisma.$disconnect();
-//   });
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+    await prisma.$disconnect();
+  });
 
-//   test("signUp should create a new user", async () => {
-//     const userData = {
-//       name: "Another User",
-//       email: generateUniqueEmail(),
-//       password: "password123",
-//       role: "user",
-//     };
-//     const response = await signUp(userData);
-//     expect(response.user.email).toBe(userData.email);
-//     expect(
-//       await bcrypt.compare(userData.password, response.user.password)
-//     ).toBe(true);
-//   });
+  test("signUp should create a new user", async () => {
+    const userData = {
+      name: "Another User",
+      email: generateUniqueEmail(),
+      password: "password123",
+      role: "user",
+    };
+    const response = await signUp(userData);
+    expect(response.user.email).toBe(userData.email);
+    expect(
+      await bcrypt.compare(userData.password, response.user.password)
+    ).toBe(true);
+  });
 
-//   test("signIn should authenticate a user", async () => {
-//     const userData = {
-//       name: "Test User",
-//       email: generateUniqueEmail(),
-//       password: "password123",
-//       role: "user",
-//     };
-//     await signUp(userData);
-//     const response = await signIn({
-//       email: userData.email,
-//       password: userData.password,
-//     });
-//     expect(response.user.email).toBe(userData.email);
-//     expect(response.token).toBeDefined();
-//   });
+  test("signIn should authenticate a user", async () => {
+    const userData = {
+      name: "Test User",
+      email: generateUniqueEmail(),
+      password: "password123",
+      role: "user",
+    };
+    await signUp(userData);
+    const response = await signIn({
+      email: userData.email,
+      password: userData.password,
+    });
+    expect(response.user.email).toBe(userData.email);
+    expect(response.accessToken).toBeDefined();
+  });
 
-//   test("getUser should return user by id", async () => {
-//     const user = await getUser(userId);
-//     expect(user).not.toBeNull();
-//     if (user) {
-//       expect(user.email).toMatch(/test_\d+@example.com/);
-//     }
-//   });
+  test("getUser should return user by id", async () => {
+    const user = await getUser(userId);
+    expect(user).not.toBeNull();
+    if (user) {
+      expect(user.email).toMatch(/test_\d+@example.com/);
+    }
+  });
 
-//   test("updateUser should update user data", async () => {
-//     const updatedData = {
-//       name: "Updated User",
-//       email: `updated_${Date.now()}@example.com`,
-//       role: "user",
-//       password: "newpassword123",
-//     };
-//     const updatedUser = await updateUser(userId, updatedData);
+  test("updateUser should update user data", async () => {
+    const updatedData = {
+      name: "Updated User",
+      email: `updated_${Date.now()}@example.com`,
+      role: "user",
+      password: "newpassword123",
+    };
+    const updatedUser = await updateUser(userId, updatedData);
 
-//     expect(updatedUser.email).toBe(updatedData.email);
-//     expect(await bcrypt.compare("newpassword123", updatedUser.password)).toBe(
-//       true
-//     );
-//   });
+    expect(updatedUser.email).toBe(updatedData.email);
+    expect(await bcrypt.compare("newpassword123", updatedUser.password)).toBe(
+      true
+    );
+  });
 
-//   test("refreshTokenService should return a new access token", async () => {
-//     const refreshToken = "some-refresh-token";
-//     await prisma.user.update({ where: { id: userId }, data: { refreshToken } });
+  test("refreshTokenService should return a new access token", async () => {
+    const refreshToken = "some-refresh-token";
+    await prisma.user.update({ where: { id: userId }, data: { refreshToken } });
 
-//     const response = await refreshTokenService(refreshToken);
-//     expect(response.newAccessToken).toBeDefined();
-//   });
+    const response = await refreshTokenService(refreshToken);
+    expect(response.newAccessToken).toBeDefined();
+  });
 
-//   test("deleteUser should remove the user", async () => {
-//     await deleteUser(userId);
-//     const user = await getUser(userId);
-//     expect(user).toBeNull();
-//   });
-// });
+  test("deleteUser should remove the user", async () => {
+    await deleteUser(userId);
+    const user = await getUser(userId);
+    expect(user).toBeNull();
+  });
+});
