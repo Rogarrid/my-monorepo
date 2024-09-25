7d2180e559a404db7155bb152372f47a48e836e7
Rocio <rocio.garrido@es.abb.com>
2024-09-25 12:21:26 +0200

    fixed dynamic port for desployment

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -2,7 +2,9 @@ import { build } from "./build";
 
 const fastify = build();
 
-fastify.listen({ port: 3000 }, (err, address) => {
+const PORT = Number(process.env.PORT) || 3000;
+
+fastify.listen({ port: PORT }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
